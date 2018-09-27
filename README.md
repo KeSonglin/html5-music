2512c6c5c81aeaa324f997d99f10e9f4c7c28421
linke <15072925663@163.com>
2018-09-27 20:56:52 +0800

    添加注释

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # html5music
 a  simple HTML5 music project 
+
+# install dependencies
+$ cnpm install / npm install
+
+# start
+$ npm start / gulp
--- src/js/playlist.js
+++ src/js/playlist.js
@@ -1,47 +1,51 @@
-(function($,root){
+(function ($, root) {
     var $scope = $(document.body);
     var control;
-    var $playList = $("<div class = 'play-list'>" +
-        "<div class='list-header'>播放列表</div>" + 
+    var $playList = $("<div class='play-list'>" +
+        "<div class='list-header'>播放列表</div>" +
         "<ul class='list-wrapper'></ul>" +
         "<div class='btn-close'>关闭</div>" +
-    "</div>") 
-    //渲染我们的播放列表dom
-    function renderList(songList){
+        "</div>");
+
+    // 渲染播放列表dom
+    function renderList(songList) {
         var html = '';
-        for(var i = 0;i < songList.length;i++){
-            html += "<li><h3 >"+songList[i].song+"-<span>"+songList[i].singer+"</span></h3></li>"
+        for (var i = 0; i < songList.length; i++) {
+            html += "<li><h3 >" + songList[i].song + "-<span>" + songList[i].singer + "</span></h3></li>"
         }
         $playList.find("ul").html(html);
         $scope.append($playList);
         bindEvent();
-    }  
-    function show(controlmanager){
+    }
+    // 在播放列表中，给当前播放的音乐添加样式
+    function show(controlmanager) {
         control = controlmanager;
         $playList.addClass("show");
         signSong(control.index);
     }
-    function bindEvent(){
-        $playList.on("click",".btn-close",function(){
+    // 绑定点击播放事件
+    function bindEvent() {
+        // 点击“关闭”， 隐藏播放列表
+        $playList.on("click", ".btn-close", function () {
             $playList.removeClass("show")
-        })
-        $playList.find("li").on("click",function(){
+        });
+        $playList.find("li").on("click", function () {
             var index = $(this).index();
             signSong(index);
             control.index = index;
-            $scope.trigger("play:change",[index,true]);
+            $scope.trigger("play:change", [index, true]);
             $scope.find(".btn-play").addClass("playing");
-            setTimeout(function(){
+            setTimeout(function () {
                 $playList.removeClass("show")
             }, 200);
-        })
+        });
     }
-    function signSong(index){
+    function signSong(index) {
         $playList.find(".sign").removeClass("sign");
         $playList.find("ul li").eq(index).addClass("sign");
     }
     root.playList = {
-        renderList : renderList,
-        show : show
-    }
-})(window.Zepto,window.player || (window.player = {}))
+        renderList: renderList,
+        show: show
+    };
+})(window.Zepto, window.player || (window.player = {}))
